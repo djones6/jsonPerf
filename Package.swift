995b06b7ff777aa981aebb0f798c803f134e9e35
David Jones <djones6@uk.ibm.com>
2017-03-28 17:20:48 +0100

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,10 @@
+// swift-tools-version:3.1
+
+import PackageDescription
+
+let package = Package(
+    name: "testproj",
+    dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1),
+        .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1)
+    ])
